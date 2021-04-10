7314182e902edcca188067b839c565392d094869
Payal <payalahuja98@gmail.com>
2021-04-10 13:57:29 -0500

    added background image and text block with styles

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { Link } from "react-router-dom";
-import heroImage from './static/Datathon Hero.png';
-import MainLogo from './static/Logo + Type@2x.png'
-import axios from 'axios';
-import Moment from 'moment';
+import uiuc_home from './uiuc_home.jpg';
 import Image from 'react-bootstrap/Image'
 
 export default class LandingPage extends React.Component {
@@ -22,85 +18,18 @@ export default class LandingPage extends React.Component {
     };
   }
 
-
-  componentDidMount() {
-    var targetUrl = 'https://wp.stanforddaily.com/wp-json/wp/v2/posts?_embed&categories=58277' 
-      // embed adds featured image
-
-    axios.get(targetUrl)
-      .then(result => {
-        let stories = result.data.slice(0, 3);
-        this.setState({
-          isLoaded: true,
-          articles: stories
-        });
-      },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-
-    axios.get('https://open-data-portal.s3.us-east-2.amazonaws.com/metadata.json')
-      .then(result => {
-          this.setState({
-            isLoaded: true,
-            items: result.data,
-          });
-        },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-      /*.then(() => {
-        let slugs = this.state.items.filter(dataset => dataset.stories !== "").slice(0, 3).map(dataset => dataset.stories);
-        console.log(this.state.items);
-        for (var i = 0; i < slugs.length; i++) {
-          let multipleSlugs = slugs[i].split(",")
-          for (var j = 0; j < multipleSlugs.length; j++) {
-            targetUrl += '&slug[]=' + multipleSlugs[j];
-          }
-        }
-        console.log(targetUrl);
-
-        fetch(targetUrl)
-        .then(blob => blob.json())
-        .then(result => {
-          this.setState({
-            isLoaded: true,
-            articles: result,
-          });
-        },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-      })*/
-  }
-
-
   render() {
-    function html_entity_decode(message) {
-      /* decodes UTF8 punctuation into HTML */
-      var element = document.createElement("div");
-      element.innerHTML = message;
-      return element.innerHTML;
-    }
-
     return (
-      <div className="home">
-        <header>
-          <div id='info'>
-          </div>
-        </header>
+      <div className="info">
+        <Image id='img'
+          src={uiuc_home} 
+          alt="Picture of University Campus"
+          fluid 
+        />
+        <div class="text-block">
+            <h4>University of Illinois at Urbana-Champaign</h4>
+            <p>Open Data Portal</p>
+        </div>
       </div>
     );
   }
