5ad203f8836cc7c75f5bd6597916686157bcfab7
Payal <payalahuja98@gmail.com>
2021-04-10 14:41:05 -0500

    implemented search bar on landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './styles.css';
 import uiuc_home from './uiuc_home.jpg';
 import Image from 'react-bootstrap/Image'
+import books from "./Datasets.json";
+import SearchBar from "./Search.js";
 
 export default class LandingPage extends React.Component {
   state = {
@@ -29,6 +31,11 @@ export default class LandingPage extends React.Component {
         <div class="text-block">
             <h4>University of Illinois at Urbana-Champaign</h4>
             <p>Open Data Portal</p>
+        </div>
+        <div class="search">
+        <SearchBar
+          
+        />
         </div>
       </div>
     );
