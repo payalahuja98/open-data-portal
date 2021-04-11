235d88e565d7e28eb9666a5fcb10110bc36c0df3
Payal <payalahuja98@gmail.com>
2021-04-11 00:49:15 -0500

    finished input form and added image to Request Data tab, made fonts, colors consistent

--- src/components/Fellowship.jsx
+++ src/components/Fellowship.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import "./fellowship.css"
 import Dropdown from 'react-bootstrap/Dropdown'
 import "./styles.css"
+import RequestImg from "./request-image.png"
+import Image from 'react-bootstrap/Image'
+import { Button } from 'react-bootstrap';
 
 function Fellowship() {
   return (
-    <div className="container">
+    <div className="request-container">
+    <div>
+    <Image id='img'
+        src={RequestImg}
+        alt="University Logo"
+        fluid 
+    />
+    </div>
+    <div className="input-form">
       <form>
             <h5>Name</h5>
             <input />
@@ -26,7 +37,27 @@ function Fellowship() {
                 <Dropdown.Item href="#/action-5">Other</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
+            <h5>Time</h5>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-time">
+                Most Recent
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">1 Year</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">5 Years</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">10 Years</Dropdown.Item>
+                <Dropdown.Item href="#/action-4">Oldest Available</Dropdown.Item>
+                <Dropdown.Item href="#/action-5">All</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <h5>Reason for requesting</h5>
+            <textarea></textarea>
+            <h5>Additional Notes</h5>
+            <textarea></textarea>
+            <Button id="submit">Submit</Button>
         </form>
+    </div>
     </div>
   )
 };
